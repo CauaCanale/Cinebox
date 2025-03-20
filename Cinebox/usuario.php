e5300e423ea7373e426a3c3d671e8afb57813bb4
CauaCanale <caua.cferreira2@gmail.com>
2025-03-20 11:53:12 -0300

    .

--- Cinebox/usuario.php
+++ Cinebox/usuario.php
@@ -2,6 +2,17 @@
 include './includes/header.php';
 
 
+if ($_SERVER ['REQUEST_METHOD'] == 'GET' && !empty($_GET) && $_GET['sair'] == 'true') {
+    session_destroy();
+    header('location:index.php');
+
+}
+
+if (empty($_SESSION) && !isset($_SESSION['id-pessoa'] ) ) {
+    header('location:usuario-login.php');
+}
+
+
 ?>
 <section id="usuario-principal">
     <main class="container usuario-principal">
